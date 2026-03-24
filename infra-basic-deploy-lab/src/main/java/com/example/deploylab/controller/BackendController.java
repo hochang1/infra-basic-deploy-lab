3fe13b5e49d930efbea09c39b2dd215504925f4b
hochang1 <lhc4254@gmail.com>
2026-03-24 09:06:56 +0900

    update

--- infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class BackendController {
-
-    @GetMapping("/backend")
+    
+	    @GetMapping("/backend")
     public String backend() {
         return "backend";
     }
@@ -22,5 +22,15 @@ public class BackendController {
     @GetMapping("/backend3")
     public String backend3() {
         return "backend3";
+    }
+    
+    @GetMapping("/backend4")
+    public String backend4() {
+        return "backend4";
+    }
+    
+    @GetMapping("/backend5")
+    public String backend5() {
+        return "backend5";
     }
 }
